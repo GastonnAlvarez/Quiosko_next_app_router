fc4d6f85058ea21ed08365795d52210c95083265
Gaston Alejandro Alvarez <gaston.alvarez.gary@gmail.com>
2024-05-15 23:30:12 -0300

    first commit

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -18,7 +18,7 @@ export default async function OrderPage({ params }: { params: { category: string
     return (
         <>
             <Heading>Elige y personaliza tu pedido a continuacion</Heading>
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 items-start">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2 items-start">
                 {products.map(product => (
                     <ProductCard
                         key={product.id}
